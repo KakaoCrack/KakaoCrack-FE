90027e1983998fcdeb513cf49bd957bbaa4a8f96
munjun0608 <munjun0608@gmail.com>
2026-01-28 14:00:56 +0900

    bgm삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@
 import "./globals.css";
 import localFont from "next/font/local";
 import MotionProvider from "./MotionProvider";
+// [추가] 배경음악 컴포넌트 불러오기
+import BackgroundMusic from "@/components/common/BackgroundMusic";
 
 const dungGeunMO = localFont({
   src: "../assets/fonts/DungGeunMO.otf",
@@ -16,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={dungGeunMO.className}>
+        {/* [추가] 배경음악 컴포넌트 배치 (페이지가 바뀌어도 유지됨) */}
+        <BackgroundMusic />
+
         <MotionProvider>{children}</MotionProvider>
       </body>
     </html>
