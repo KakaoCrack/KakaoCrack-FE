a40da3e4f37ffbf96ceb86acc66fd8f7f8ac08c1
munjun0608 <munjun0608@gmail.com>
2026-02-02 15:55:04 +0900

    아이템 설명 수정

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -31,7 +31,7 @@ const ITEMS: Item[] = [
     id: "fur",
     name: "갈색 털뭉치",
     description:
-      "갈색 털뭉치\n\n누군가가 떨어뜨린\n갈색 털뭉치이다.\n갈색 털뭉치의 것일까?",
+      "갈색 털뭉치\n\n누군가가 떨어뜨린\n갈색 털뭉치이다.\n갈색 털동물의 것일까?",
     icon: "/character/아이템_갈색털뭉치.svg",
     miniIcon: "/character/아이템_갈색털뭉치_미니.svg",
   },
@@ -47,7 +47,7 @@ const ITEMS: Item[] = [
     id: "chocolate",
     name: "초콜릿 봉지",
     description:
-      "초콜릿 봉지\n\n누군가가 떨어뜨린\n초콜릿 봉지이다.\n탐식실에 비치된 초콜릿과\n동일한 브랜드이다.",
+      "초콜릿 봉지\n\n누군가가 떨어뜨린\n초콜릿 봉지이다.\n탕비실에 비치된 초콜릿과\n동일한 브랜드이다.",
     icon: "/character/아이템_초콜릿봉지.svg",
     miniIcon: "/character/아이템_초콜릿봉지_미니.svg",
   },
@@ -55,7 +55,7 @@ const ITEMS: Item[] = [
     id: "coffee",
     name: "커피 자국",
     description:
-      "커피 자국\n\n누군가가 커피를 흘린 자국이\n제대로 지워지지 않고\n희미하게 남아 있었다.\n어피치의 동선을 추적 의도했다.",
+      "커피 자국\n\n누군가가 커피를 흘린 자국이\n제대로 지워지지 않고\n희미하게 남아 있었다.\n어피치의 증언을 통해 획득했다.",
     icon: "/character/아이템_커피자국.svg",
     miniIcon: "/character/아이템_커피자국_미니.svg",
   },
